39c117fd450f64b9bb751d4a4c7d249dfce32213
Sonnguyenmin <nguyensoncl184@gmail.com>
2024-10-08 14:46:21 +0700

    son10

--- src/main/java/sq/project/md5/perfumer/model/dto/req/UserRequest.java
+++ src/main/java/sq/project/md5/perfumer/model/dto/req/UserRequest.java
@@ -3,6 +3,7 @@ package sq.project.md5.perfumer.model.dto.req;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
--- src/main/java/sq/project/md5/perfumer/model/dto/resp/UserResponse.java
+++ src/main/java/sq/project/md5/perfumer/model/dto/resp/UserResponse.java
@@ -3,6 +3,7 @@ package sq.project.md5.perfumer.model.dto.resp;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
+import sq.project.md5.perfumer.model.entity.Users;
 
 import java.util.Date;
 
@@ -26,4 +27,17 @@ public class UserResponse {
     @Temporal(TemporalType.DATE)
     private Date updatedAt;
     private String isDeleted;
+
+    public UserResponse(Users users) {
+        this.username = users.getUsername();
+        this.email = users.getEmail();
+        this.fullName = users.getFullName();
+        this.address = users.getAddress();
+        this.phone = users.getPhone();
+        this.createdAt = users.getCreatedAt();
+        this.updatedAt = users.getUpdatedAt();
+        this.isDeleted = users.getIsDeleted().toString();
+        this.avatar = users.getAvatar();
+        this.status = users.getStatus().toString();
+    }
 }
